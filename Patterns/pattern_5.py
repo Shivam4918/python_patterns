d5f1223e31f043d9b6fd793db88e2364d22dcf60
shivam491 <shivam4918@gmail.com>
2026-01-16 07:01:25 +0530

    Added Pattern_5 program

--- Patterns/pattern_5.py
+++ Patterns/pattern_5.py
@@ -1,7 +1,11 @@
-ttern5(n):
+# for i in range(5,0,-1):
+#     for j in range(5,0,-1):
+#         print(j,end="")
+#     print()
+
+def pattern5(n):
     for i in range(n,0,-1):
         for j in range(n,0,-1):
             print(j,end="")
         print()
-
 pattern5(5)
