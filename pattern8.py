9c256ea160366d5e9496b2e7ec5d72607ecc999c
shivam491 <shivam4918@gmail.com>
2026-01-18 21:05:49 +0530

    Added Pattern_8 program

---
+++ pattern8.py
@@ -0,0 +1,11 @@
+# for row in range():
+#     for column in range():
+#         print()
+#     print()
+
+def pattern8(number):
+    for row in range(65,70):
+        for column in range(65,70):
+            print(chr(row),end=" ")
+        print()
+pattern8(5)
