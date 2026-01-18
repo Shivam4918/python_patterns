b93ccdb689d9a6c4a0824c0a8d86eff1eb125ebc
shivam491 <shivam4918@gmail.com>
2026-01-18 21:28:16 +0530

    Added Pattern_11 program

---
+++ Patterns/pattern_11.py
@@ -0,0 +1,11 @@
+# for row in range(69,64,-1):
+#     for column in range(69,64,-1):
+#         print(chr(column),end="")
+#     print()
+
+def pattern8(number):
+    for row in range(69,64,-1):
+        for column in range(69,64,-1):
+            print(chr(column),end="")
+        print()
+pattern8(5)
