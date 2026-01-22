f76d654b3fa79a2dac9b6e5160007cf8a7c91a7d
shivam491 <shivam4918@gmail.com>
2026-01-22 10:55:27 +0530

    Added Pattern_18 program

---
+++ Patterns/pattern_18.py
@@ -0,0 +1,16 @@
+count = 2
+for row in range(1,6):
+    for column in range(1,6):
+        print(count,end=" ")
+        count+=2
+    print()
+
+
+# def pattern18(n):
+#     count = 1
+#     for row in range(1,n+1):
+#         for column in range(1,n+1):
+#             print(count,end=" ")
+#             count+=2
+#         print()
+# pattern18(5)
